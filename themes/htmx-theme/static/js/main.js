6e07df7d41ac63cc215ee325946ac48582f50862
Travis Wagner <travis.wagner@umusic.com>
2024-04-24 20:39:16 -0400

    do js things with tables

---
+++ themes/htmx-theme/static/js/main.js
@@ -0,0 +1,20 @@
+// general site stuff
+const infoTables = document.querySelectorAll('table');
+infoTables.forEach((table) => {
+    const tds = Array.from(table.querySelectorAll('td'))
+    tds.forEach((td) => {
+        // wrap the inner content in a div
+        const div = document.createElement('div');
+        div.innerHTML = td.innerHTML;
+        td.innerHTML = '';
+        td.appendChild(div);
+    });
+    // head the headers
+    const thead = table.querySelector('thead');
+    console.log({ thead });
+    if (!thead) return;
+    const headers = Array.from(thead.querySelectorAll('th')).map(th => th.textContent);
+    console.log({ headers });
+    // for each cell in the table, add a data-label attribute with the header value
+    tds.forEach(td => td.setAttribute('data-label', headers[td.cellIndex]));
+});
